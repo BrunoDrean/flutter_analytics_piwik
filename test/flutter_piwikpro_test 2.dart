2a13231797ba4014979c5e1dd94903cfe9d6c8ca
Bruno DREAN <bruno.drean@niji.fr>
2022-10-25 22:54:48 +0200

    fix tests

--- test/flutter_piwikpro_test 2.dart
+++ test/flutter_piwikpro_test 2.dart
@@ -1,103 +1,103 @@
 import 'package:flutter/services.dart';
-import 'package:flutter_piwikpro/model/custom_variable_scope.dart';
+import 'package:flutter_analytics_piwik/flutter_piwikpro.dart';
+import 'package:flutter_analytics_piwik/model/custom_variable_scope.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_piwikpro/flutter_piwikpro.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('piwik_flutter');
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  const _configureTrackerResult = 'configured the tracker';
-  const _sendViewResult = 'sent a view';
-  const _setAnonymizationStateResult = 'set anonymization';
-  const _trackCustomEventResult = 'sent a custom event';
-  const _trackExceptionResult = 'tracked an exception';
-  const _trackSocialInteractionResult = 'tracked a social interaction';
-  const _trackDownloadResult = 'tracker a download';
-  const _trackAppInstallResult = 'tracked an app install';
-  const _trackOutlinkResult = 'tracked an outlink';
-  const _trackSearchResult = 'tracked a search';
-  const _trackContentImpressionResult = 'tracked a content impression';
-  const _trackContentInteractionResult = 'tracked a content interaciton';
-  const _trackGoalResult = 'tracked a goal';
-  const _trackEcommerceTransactionResult = 'tracked an ecommerce transaction';
-  const _trackCampaignResult = 'tracked a campaign';
-  const _trackCustomVariableResult = 'tracked a custom variable';
-  const _trackCustomDimensionResult = 'tracked a custom dimension';
-  const _trackProfileAttributeResult = 'tracked a profile attribute';
-  const _readUserProfileAttributesResult = {'testName': 'testValue'};
-  const _checkAudienceMembershipResult = true;
-  const _setUserIdResult = 'set a user id';
-  const _setUserEmailResult = 'set a user email';
-  const _setVisitorIdResult = 'set a visitorId';
-  const _setSessionTimeoutResult = 'set a session timeout';
-  const _setDispatchIntervalResult = 'set a dispatch interval';
-  const _dispatchResult = 'dispatched all events from the queue';
-  const _setIncludeDefaultVariablesResult = 'set whether default variables should be included';
-  const _optOutResult = 'opted out';
-  const _dryRunResult = 'dry run enabled';
+  const configureTrackerResult = 'configured the tracker';
+  const sendViewResult = 'sent a view';
+  const setAnonymizationStateResult = 'set anonymization';
+  const trackCustomEventResult = 'sent a custom event';
+  const trackExceptionResult = 'tracked an exception';
+  const trackSocialInteractionResult = 'tracked a social interaction';
+  const trackDownloadResult = 'tracker a download';
+  const trackAppInstallResult = 'tracked an app install';
+  const trackOutlinkResult = 'tracked an outlink';
+  const trackSearchResult = 'tracked a search';
+  const trackContentImpressionResult = 'tracked a content impression';
+  const trackContentInteractionResult = 'tracked a content interaciton';
+  const trackGoalResult = 'tracked a goal';
+  const trackEcommerceTransactionResult = 'tracked an ecommerce transaction';
+  const trackCampaignResult = 'tracked a campaign';
+  const trackCustomVariableResult = 'tracked a custom variable';
+  const trackCustomDimensionResult = 'tracked a custom dimension';
+  const trackProfileAttributeResult = 'tracked a profile attribute';
+  const readUserProfileAttributesResult = {'testName': 'testValue'};
+  const checkAudienceMembershipResult = true;
+  const setUserIdResult = 'set a user id';
+  const setUserEmailResult = 'set a user email';
+  const setVisitorIdResult = 'set a visitorId';
+  const setSessionTimeoutResult = 'set a session timeout';
+  const setDispatchIntervalResult = 'set a dispatch interval';
+  const dispatchResult = 'dispatched all events from the queue';
+  const setIncludeDefaultVariablesResult = 'set whether default variables should be included';
+  const optOutResult = 'opted out';
+  const dryRunResult = 'dry run enabled';
 
   setUp(() {
     channel.setMockMethodCallHandler((MethodCall methodCall) async {
       switch (methodCall.method) {
         case FlutterPiwikPro.configureTrackerName:
-          return _configureTrackerResult;
+          return configureTrackerResult;
         case FlutterPiwikPro.trackScreenName:
-          return _sendViewResult;
+          return sendViewResult;
         case FlutterPiwikPro.setAnonymizationStatenName:
-          return _setAnonymizationStateResult;
+          return setAnonymizationStateResult;
         case FlutterPiwikPro.trackCustomEventName:
-          return _trackCustomEventResult;
+          return trackCustomEventResult;
         case FlutterPiwikPro.trackExceptionName:
-          return _trackExceptionResult;
+          return trackExceptionResult;
         case FlutterPiwikPro.trackSocialInteractionName:
-          return _trackSocialInteractionResult;
+          return trackSocialInteractionResult;
         case FlutterPiwikPro.trackOutlinkName:
-          return _trackOutlinkResult;
+          return trackOutlinkResult;
         case FlutterPiwikPro.trackSearchName:
-          return _trackSearchResult;
+          return trackSearchResult;
         case FlutterPiwikPro.trackDownloadName:
-          return _trackDownloadResult;
+          return trackDownloadResult;
         case FlutterPiwikPro.trackAppInstallName:
-          return _trackAppInstallResult;
+          return trackAppInstallResult;
         case FlutterPiwikPro.trackContentImpressionName:
-          return _trackContentImpressionResult;
+          return trackContentImpressionResult;
         case FlutterPiwikPro.trackContentInteractionName:
-          return _trackContentInteractionResult;
+          return trackContentInteractionResult;
         case FlutterPiwikPro.trackGoalName:
-          return _trackGoalResult;
+          return trackGoalResult;
         case FlutterPiwikPro.trackEcommerceTransactionName:
-          return _trackEcommerceTransactionResult;
+          return trackEcommerceTransactionResult;
         case FlutterPiwikPro.trackCampaignName:
-          return _trackCampaignResult;
+          return trackCampaignResult;
         case FlutterPiwikPro.trackCustomVariableName:
-          return _trackCustomVariableResult;
+          return trackCustomVariableResult;
         case FlutterPiwikPro.trackCustomDimensionName:
-          return _trackCustomDimensionResult;
+          return trackCustomDimensionResult;
         case FlutterPiwikPro.trackProfileAttributeName:
-          return _trackProfileAttributeResult;
+          return trackProfileAttributeResult;
         case FlutterPiwikPro.readUserProfileAttributesName:
-          return _readUserProfileAttributesResult;
+          return readUserProfileAttributesResult;
         case FlutterPiwikPro.checkAudienceMembershipName:
-          return _checkAudienceMembershipResult;
+          return checkAudienceMembershipResult;
         case FlutterPiwikPro.setUserIdName:
-          return _setUserIdResult;
+          return setUserIdResult;
         case FlutterPiwikPro.setUserEmailName:
-          return _setUserEmailResult;
+          return setUserEmailResult;
         case FlutterPiwikPro.setVisitorIdName:
-          return _setVisitorIdResult;
+          return setVisitorIdResult;
         case FlutterPiwikPro.setSessionTimeoutName:
-          return _setSessionTimeoutResult;
+          return setSessionTimeoutResult;
         case FlutterPiwikPro.setDispatchIntervalName:
-          return _setDispatchIntervalResult;
+          return setDispatchIntervalResult;
         case FlutterPiwikPro.dispatchName:
-          return _dispatchResult;
+          return dispatchResult;
         case FlutterPiwikPro.setIncludeDefaultVariablesName:
-          return _setIncludeDefaultVariablesResult;
+          return setIncludeDefaultVariablesResult;
         case FlutterPiwikPro.optOutName:
-          return _optOutResult;
+          return optOutResult;
         case FlutterPiwikPro.dryRunName:
-          return _dryRunResult;
+          return dryRunResult;
       }
     });
   });
@@ -107,160 +107,153 @@ void main() {
   });
 
   test('configureTracker - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance
-        .configureTracker(baseURL: 'baseURL', siteId: 'siteId');
-    expect(result, equals(_configureTrackerResult));
+    final result = await FlutterPiwikPro.sharedInstance.configureTracker(baseURL: 'baseURL', siteId: 'siteId');
+    expect(result, equals(configureTrackerResult));
   });
 
   test('setAnonymizationState - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setAnonymizationState(true);
-    expect(result, equals(_setAnonymizationStateResult));
+    expect(result, equals(setAnonymizationStateResult));
   });
 
   test('trackScreen - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.trackScreen(screenName: 'screenName');
-    expect(result, equals(_sendViewResult));
+    expect(result, equals(sendViewResult));
   });
 
   test('trackCustomEvent - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance
-        .trackCustomEvent(action: 'action', category: 'category');
-    expect(result, equals(_trackCustomEventResult));
+    final result = await FlutterPiwikPro.sharedInstance.trackCustomEvent(action: 'action', category: 'category');
+    expect(result, equals(trackCustomEventResult));
   });
 
   test('trackException - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance
-        .trackException(description: 'description', isFatal: false);
-    expect(result, equals(_trackExceptionResult));
+    final result = await FlutterPiwikPro.sharedInstance.trackException(description: 'description', isFatal: false);
+    expect(result, equals(trackExceptionResult));
   });
 
   test('trackSocialInteraction - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance
-        .trackSocialInteraction(interaction: 'interaction', network: 'network');
-    expect(result, equals(_trackSocialInteractionResult));
+    final result =
+        await FlutterPiwikPro.sharedInstance.trackSocialInteraction(interaction: 'interaction', network: 'network');
+    expect(result, equals(trackSocialInteractionResult));
   });
 
   test('trackOutlink - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.trackOutlink('https://testurl.com');
-    expect(result, equals(_trackOutlinkResult));
+    expect(result, equals(trackOutlinkResult));
   });
 
   test('trackSearch - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance
         .trackSearch(keyword: 'Keyword', category: 'Best category', numberOfHits: 10);
-    expect(result, equals(_trackSearchResult));
+    expect(result, equals(trackSearchResult));
   });
 
   test('trackDownload - correct method is called', () async {
-    final result =
-        await FlutterPiwikPro.sharedInstance.trackDownload('https://averyniceurl.com');
-    expect(result, equals(_trackDownloadResult));
+    final result = await FlutterPiwikPro.sharedInstance.trackDownload('https://averyniceurl.com');
+    expect(result, equals(trackDownloadResult));
   });
 
   test('trackAppInstall - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.trackAppInstall();
-    expect(result, equals(_trackAppInstallResult));
+    expect(result, equals(trackAppInstallResult));
   });
 
   test('trackContentImpression - correct method is called', () async {
-    final result =
-        await FlutterPiwikPro.sharedInstance.trackContentImpression(contentName: 'best content');
-    expect(result, equals(_trackContentImpressionResult));
+    final result = await FlutterPiwikPro.sharedInstance.trackContentImpression(contentName: 'best content');
+    expect(result, equals(trackContentImpressionResult));
   });
 
   test('trackContentInteraction - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance
         .trackContentInteraction(contentName: 'bestContent', contentInteraction: 'clicked');
-    expect(result, equals(_trackContentInteractionResult));
+    expect(result, equals(trackContentInteractionResult));
   });
 
   test('trackGoal - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.trackGoal(goal: 10, revenue: 100.0);
-    expect(result, equals(_trackGoalResult));
+    expect(result, equals(trackGoalResult));
   });
 
   test('trackEcommerceTransaction - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance
-        .trackEcommerceTransaction(identifier: 'testID', grandTotal: 100);
-    expect(result, equals(_trackEcommerceTransactionResult));
+    final result =
+        await FlutterPiwikPro.sharedInstance.trackEcommerceTransaction(identifier: 'testID', grandTotal: 100);
+    expect(result, equals(trackEcommerceTransactionResult));
   });
 
   test('trackCampaign - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance.trackCampaign(
-        "http://example.org/offer.html?pk_campaign=Email-SummerDeals&pk_keyword=LearnMore");
-    expect(result, equals(_trackCampaignResult));
+    final result = await FlutterPiwikPro.sharedInstance
+        .trackCampaign("http://example.org/offer.html?pk_campaign=Email-SummerDeals&pk_keyword=LearnMore");
+    expect(result, equals(trackCampaignResult));
   });
 
   test('trackCustomVariable - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance.trackCustomVariable(
-        index: 1, name: 'good name', value: 'best value', scope: CustomVariableScope.action);
-    expect(result, equals(_trackCustomVariableResult));
+    final result = await FlutterPiwikPro.sharedInstance
+        .trackCustomVariable(index: 1, name: 'good name', value: 'best value', scope: CustomVariableScope.action);
+    expect(result, equals(trackCustomVariableResult));
   });
 
   test('trackCustomDimension - correct method is called', () async {
-    final result =
-        await FlutterPiwikPro.sharedInstance.trackCustomDimension(index: 5, value: 'best value');
-    expect(result, equals(_trackCustomDimensionResult));
+    final result = await FlutterPiwikPro.sharedInstance.trackCustomDimension(index: 5, value: 'best value');
+    expect(result, equals(trackCustomDimensionResult));
   });
 
   test('trackProfileAttribute - correct method is called', () async {
-    final result = await FlutterPiwikPro.sharedInstance
-        .trackProfileAttribute(name: 'testName', value: 'testValue');
-    expect(result, equals(_trackProfileAttributeResult));
+    final result = await FlutterPiwikPro.sharedInstance.trackProfileAttribute(name: 'testName', value: 'testValue');
+    expect(result, equals(trackProfileAttributeResult));
   });
 
   test('readProfileAttributes - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.readUserProfileAttributes();
-    expect(result, equals(_readUserProfileAttributesResult));
+    expect(result, equals(readUserProfileAttributesResult));
   });
 
   test('checkAudienceMembership - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.checkAudienceMembership('testId');
-    expect(result, equals(_checkAudienceMembershipResult));
+    expect(result, equals(checkAudienceMembershipResult));
   });
 
   test('setUserId - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setUserId('testId');
-    expect(result, equals(_setUserIdResult));
+    expect(result, equals(setUserIdResult));
   });
 
   test('setUserEmail - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setUserEmail('testEmail@test.test');
-    expect(result, equals(_setUserEmailResult));
+    expect(result, equals(setUserEmailResult));
   });
 
   test('setVisitorId - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setVisitorId('testVisitorId');
-    expect(result, equals(_setVisitorIdResult));
+    expect(result, equals(setVisitorIdResult));
   });
 
   test('setSessionTimeout - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setSessionTimeout(1800);
-    expect(result, equals(_setSessionTimeoutResult));
+    expect(result, equals(setSessionTimeoutResult));
   });
 
   test('setDispatchInterval - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setDispatchInterval(1800);
-    expect(result, equals(_setDispatchIntervalResult));
+    expect(result, equals(setDispatchIntervalResult));
   });
 
   test('dispatch - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.dispatch();
-    expect(result, equals(_dispatchResult));
+    expect(result, equals(dispatchResult));
   });
 
   test('setIncludeDefaultVariables - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.setIncludeDefaultVariables(false);
-    expect(result, equals(_setIncludeDefaultVariablesResult));
+    expect(result, equals(setIncludeDefaultVariablesResult));
   });
 
   test('optOut - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.optOut(true);
-    expect(result, equals(_optOutResult));
+    expect(result, equals(optOutResult));
   });
 
   test('dryRun - correct method is called', () async {
     final result = await FlutterPiwikPro.sharedInstance.dryRun(true);
-    expect(result, equals(_dryRunResult));
+    expect(result, equals(dryRunResult));
   });
 }
